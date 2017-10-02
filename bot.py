1d7e24c76832fb441bbcea2ac7d23e629f95f949
Ilie Cebanu <cebanu5665@gmail.com>
2017-10-03 00:22:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,21 +16,21 @@ async def on_message(message):
 		try:
 			deg = re.findall('\d+', message.content)[0]
 			fah = float(deg) * 9/5 + 32
-			result = '{.2f}C = {.2f}F'.format(deg, fah)
+			result = '{:.2f}C = {:.2f}F'.format(float(deg), fah)
 			print(result)
 			tmp = await client.send_message(message.channel, result)
-		except:
-			pass
+		except IndexError as e:
+			print(e)
 
 	elif message.content.startswith('!conv') and 'F' in message.content:
 		try:
 			fah = re.findall('\d+', message.content)[0]
 			deg = (float(fah) - 32) * 5/9
-			result = '{.2f}F = {.2f}C'.format(fah, deg)
+			result = '{:.2f}F = {:.2f}C'.format(float(fah), deg)
 			print(result)
 			tmp = await client.send_message(message.channel, result)
-		except:
-			pass
+		except IndexError as e:
+			print(e)
 
 
 client.run('bot_token')
